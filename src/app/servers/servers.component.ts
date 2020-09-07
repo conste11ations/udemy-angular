c01d3df935fdbcbfcf8c9955b9155959b4c03953
Rachel Kat <rachel.kat@koho.ca>
2020-09-06 22:23:42 -0400

    angular section 2 part 22

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // tslint:disable-next-line: component-selector
+  // selector: '[app-servers]',
+  // tslint:disable-next-line: component-selector
+  // selector: '.app-servers',
+  template: `<app-server></app-server><app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
